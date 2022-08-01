b3a356733c29462cc5cb9ef823945416d0773923
@tahani-cherif <tahani.cherif@avec.fr>
2022-08-01 14:15:47 +0100

    git commit -m

--- src/projet/copprojet.js
+++ src/projet/copprojet.js
@@ -4,7 +4,7 @@ import {BsYoutube,BsGithub} from 'react-icons/bs'
 export default function Projet(props){
     return(
             <div className='nomprojet'>
-                <img src={props.src} id='img' style={{width:props.taill}}/>
+                <img src={props.src} id='img' style={{width:props.taill}} className="img"/>
                 <p className='textprojet'>{props.nom}</p>
                 <div className='href'>              
                 <a href={props.git} target="_blank"><BsGithub color={props.colorgit}fontSize='2rem'  className="icon"/> </a> 

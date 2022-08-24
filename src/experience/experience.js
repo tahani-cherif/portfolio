900b4ee3d154e2363416e70d7f7090420afc9bb1
@tahani-cherif <tahani.cherif@avec.fr>
2022-08-24 12:03:04 +0100

    git commit -m

--- src/experience/experience.js
+++ src/experience/experience.js
@@ -2,19 +2,41 @@ import ".//experience.css"
  export default function Experience(){
       return(<div className="exp" id="experience">
              <p className="textexp">Expériences professionnelles</p>
-             <ul>
-             <li>
-               <b> Durée :</b> Travail actuel
-                </li>
-                <p  className="disc">Poste de Développeur Full Stack chez WAZOLAB.</p>
-                <li>
-               <b> Durée:</b> 1 février au 30 avril 2022
-                </li>
-                <p className="disc">Stage en développement mobile au StartDevelopment.</p>
-                <li>
-               <b> Durée :</b> Le mois du juillet 2021 
-                </li>
-                <p  className="disc">Stage en Blockchain au Centre D'innovation.</p>
-             </ul>
+            <div className="post">
+            <img src="./wazolab.png" className="logo"/>
+            <div className="desc">
+            <p style={{marginLeft: "-109px",color:"#2B7CD3"}}>WAZOLAB</p>
+            <ul className="liste">
+              <li className="li">
+                 Emploi temps plein juin 2022-present .    
+              </li>
+              <li className="li">Développeur Full Stack</li>
+            </ul>
+            </div>
+            </div>
+            <div className="post">
+            <img src="./startdev.png"className="logo" />
+            <div className="desc">
+            <p style={{marginLeft: "-109px",color:"#2B7CD3"}}>StartDevelopment</p>
+            <ul className="liste">
+              <li className="li">
+                 Stage fin d'étude 1 février au 30 avril 2022 .    
+              </li>
+              <li className="li">Développement mobile</li>
+            </ul>
+            </div>
+            </div>
+            <div className="post">
+            <img src="./centre.png"className="logo" />
+            <div className="desc">
+            <p style={{marginLeft: "-80px" ,color:"#2B7CD3"}}>Centre D'innovation</p>
+            <ul className="liste">
+              <li className="li">
+              Stage fin d'année Le mois du juillet 2021 .    
+              </li>
+              <li className="li">Blockchain</li>
+            </ul>
+            </div>
+            </div>
          </div>)
   }

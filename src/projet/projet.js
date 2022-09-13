f12f7aa5b16bf3bd8d8dfee1de1f57d97a334e01
@tahani-cherif <tahani.cherif@avec.fr>
2022-09-13 10:09:24 +0100

    git commit -m "mise a jour"

--- src/projet/projet.js
+++ src/projet/projet.js
@@ -22,22 +22,27 @@ export default function Mainprojet(){
             <div className='boxprojet'>
             <Projet src="./projetexpress.png" nom="CEUD avec express js et ORM sequelize" taill="446px" 
             git="https://github.com/tahani-cherif/mini-projet-express" coloryout='#808080' colorpdf='#808080' colorgit='#2B7CD3'/>
-            <Projet src='./hrc.jpg' nom='Création d’une site web dans le cadre d’informer , communiquer et sensibiliser' 
-            taill="345px" href="https://youtu.be/yR25V80yMgM"  pdf='./harcèlement sexuelle.pdf' git="https://github.com/tahani-cherif/Hope-WebSite"
-            colorgit='#2B7CD3' colorpdf='#2B7CD3' coloryout='#2B7CD3'/>
+            <Projet src="./nextjs.png" nom="Création première  Application avec Nextjs E-commerce  (utilisée context API ,localstorage)" taill="470px" 
+            git="https://github.com/tahani-cherif/App-nextjs-context-API" colorgit='#2B7CD3' colorpdf='#808080' coloryout='#808080'/>
+            </div>
+            <div className='boxprojet'>
+             <Projet src="./formulaire.png" nom="Validation formulaire en React à l’aide de React Hook Form et Yup" taill="470px" 
+            git="https://github.com/tahani-cherif/Validation-formulaire-en-React-l-aide-de-React-Hook-Form-et-Yup" colorgit='#2B7CD3' colorpdf='#808080' coloryout='#808080'/>
+             <Projet src="./python.png" nom="Développement d'un plan de décision avec pygrametl (ETL python ) et power BI"
+             taill="489px" colorgit='#2B7CD3' colorpdf='#808080' coloryout='#808080' git="https://github.com/tahani-cherif/D-veloppement-d-un-plan-de-d-cision-avec-pygrametl-ETL-python-et-power-BI"/>
             </div>
             <div className='boxprojet'>
             <Projet src='./site.jpg' nom="Création d'une site web pour un club robotique d'enfant" taill="345px"
              pdf='php.pdf' href="https://youtu.be/09KtDCeutbM" git="https://github.com/tahani-cherif/Robotics-Club-WebSite-PHP"
              colorgit='#2B7CD3' colorpdf='#2B7CD3' coloryout='#2B7CD3'/>
-            <Projet src="./python.png" nom="Développement d'un plan de décision avec pygrametl (ETL python ) et power BI"
-             taill="489px" colorgit='#2B7CD3' colorpdf='#808080' coloryout='#808080' git="https://github.com/tahani-cherif/D-veloppement-d-un-plan-de-d-cision-avec-pygrametl-ETL-python-et-power-BI"/>
+           <Projet src='./hrc.jpg' nom='Création d’une site web dans le cadre d’informer , communiquer et sensibiliser' 
+            taill="345px" href="https://youtu.be/yR25V80yMgM"  pdf='./harcèlement sexuelle.pdf' git="https://github.com/tahani-cherif/Hope-WebSite"
+            colorgit='#2B7CD3' colorpdf='#2B7CD3' coloryout='#2B7CD3'/>
+          
             </div>
             <div className='boxprojet'>
             <Projet src="./petshop.png" nom="Développement d'un site web marchand (Animalerie) avec Wix" taill="446px" 
             href="https://youtu.be/iJ2heZGCqvQ" colorgit='#808080' colorpdf='#808080' coloryout='#2B7CD3'/>
-             <Projet src="./formulaire.png" nom="Validation formulaire en React à l’aide de React Hook Form et Yup" taill="470px" 
-            git="https://github.com/tahani-cherif/Validation-formulaire-en-React-l-aide-de-React-Hook-Form-et-Yup" colorgit='#2B7CD3' colorpdf='#808080' coloryout='#808080'/>
             </div>
         </div>
     )

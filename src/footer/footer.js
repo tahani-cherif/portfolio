265599597ad0b15f60aa73cf447f9ffcccbd9f1a
Tahani Cherif <tahani.cherif@steps.tn>
2023-09-15 09:24:36 +0100

    test share

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,8 +1,18 @@
-import './/footer.css'
-export default function Footer(){
-    return(
-        <footer >
-            <p className="footer">© Copyright 2022 - Tahani Cherif</p>
-        </footer>
-    )
+import ".//footer.css";
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+} from "react-share";
+export default function Footer() {
+  const x = window.location.href;
+  return (
+    <footer>
+      <FacebookShareButton url={x}>
+        <div style={{ color: "#066" }}>test fb</div>
+      </FacebookShareButton>
+
+      <p className="footer">© Copyright 2022 - Tahani Cherif</p>
+    </footer>
+  );
 }

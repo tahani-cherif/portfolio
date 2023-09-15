57d25fdd49335884e2634cb9e17f8a7e2d69c067
Tahani Cherif <tahani.cherif@steps.tn>
2023-09-15 09:35:10 +0100

    refactoring code

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,17 +1,9 @@
 import ".//footer.css";
-import {
-  FacebookShareButton,
-  LinkedinShareButton,
-  TwitterShareButton,
-} from "react-share";
+
 export default function Footer() {
   const x = window.location.href;
   return (
     <footer>
-      <FacebookShareButton url={x}>
-        <div style={{ color: "#066" }}>test fb</div>
-      </FacebookShareButton>
-
       <p className="footer">© Copyright 2022 - Tahani Cherif</p>
     </footer>
   );
